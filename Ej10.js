1ac856e21615c34579a6be0957c659b718932354
LRoxanaL <109041542+LRoxanaL@users.noreply.github.com>
2022-07-17 16:32:22 -0300

    Add files via upload

---
+++ Ej10.js
@@ -0,0 +1,56 @@
+alert("¡¡¡Hola!!!");
+let  nombre  = "Laura Roxana";
+let  apellido = "Lopez"
+let  saludo = "Bienvenidos a mi sitio";
+let  fraseCompleta = saludo + nombre + apellido;
+console.log (fraseCompleta);
+let  frase = "JavaScript"+"es un lenguaje"+"esencial para crear"+'sitios web poderosos';
+console.log ( frase );
+
+
+
+let  numeroString1 = "10";
+let  numeroString2 = "20";
+let  numeroString3 = "30";
+let  concatenaNumeros = numeroString1 + numeroString2 + numeroString3 ;
+console.log( concatenaNumeros );
+
+let edad = prompt ('cual es tu edad?');
+console.log (edad) ;
+
+
+sexo = prompt('introduce tu sexo');
+
+if ( sexo == 'masculino' );{
+alert ('podes entrar si sos mayor de 18') ;
+   let edad;
+   edad = prompt("Introduce tu edad");
+   if (edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.");
+   } else {
+    alert("No puedes entrar, eres menor de edad.");
+   }
+ }
+
+
+
+
+ if ( sexo == 'femenino'); {
+alert ('mujeres solo mayores de 20');
+
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >= 20){
+ alert("Puedes entrar, eres mayor de edad.");
+} else {
+ alert("No puedes entrar, eres menor de edad.");
+}
+
+
+
+
+
+}
+más; {
+    alert('verifica si has ingresado tu sexo correctamente');
+}
